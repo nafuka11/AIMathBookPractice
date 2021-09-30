87ebc832d3780798ac479d70c0a81ba382d81c57
nafuka <42476527+nafuka11@users.noreply.github.com>
2021-09-30 23:38:53 +0900

    fix(chapter6): make directories with download_rawdata.sh

--- chapter6/download_rawdata.sh
+++ chapter6/download_rawdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p dataset/raw_datas/
 curl -o dataset/raw_datas/akutagawa.txt https://raw.githubusercontent.com/TeamAidemy/AIMathBook/master/02_nlp/dataset/raw_datas/akutagawa.txt
 curl -o dataset/raw_datas/dazai.txt https://raw.githubusercontent.com/TeamAidemy/AIMathBook/master/02_nlp/dataset/raw_datas/dazai.txt
 curl -o dataset/raw_datas/mori.txt https://raw.githubusercontent.com/TeamAidemy/AIMathBook/master/02_nlp/dataset/raw_datas/mori.txt
